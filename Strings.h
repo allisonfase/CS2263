636c5dd45709eeded5bced4661591740ea3925a9
allisonfase <43859718+allisonfase@users.noreply.github.com>
2022-03-19 19:12:42 -0300

    Add files via upload

--- Strings.h
+++ Strings.h
@@ -1,6 +1,8 @@
+
 #ifndef STRINGS_H
 #define STRINGS_H
 #include<stdio.h>
+
 typedef char* String;
 
 // a cover function for malloc()
@@ -9,25 +11,28 @@ typedef char* String;
 char* mallocString(int stringsize);
 
 // just a cover function for free()
-void freeString(String s);
+void freeString(char* s);
 
 // create a duplicate string of s
 // return it
 // return (char*)NULL on failure
 // should call mallocString(), and then strcpy()
-char* duplicateString(String s);
+char* duplicateString(char* s);
 
 // create a duplicate of string list sl
 // return it
-// return (char**)NULL on failure
+// return (char**)Null on failure
 // uses other Strings module functions
-char** duplicateStringList(String* sl,int n);
+char** duplicateStringList(char** sl, int n);
 
 // Return an allocated string from an open file,
 // Stop reading when any character is in terminators list
-// return allocated string or (char*)NULL
-char* getfString(FILE* pFIn, String terminators);
-char* getString(String terminators);
+// return allocated struing or (char*)NULL
+char* getfString(FILE* pFIn, char* terminators);
+char* getString(char* terminators, int n);
+
 void sortStringList(String strings[], int n);
+
 int compareStrings(const void* a, const void* b);
+
 #endif
